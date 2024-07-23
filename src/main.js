e5ba1b9ab3a1db2b8981800b00685200a6c3ea46
Tumelo Mogono <tumelomogono@gmail.com>
2024-07-23 16:22:12 +0200

    Added new initial state values for filteredProducts which will be updated with the products array, sorted array and filtered array. I then have sortOption set to default which will change with values of the sort options and filterOptions which will change with the values we get from the filterData filterOptions. Inside the init I also added an argument being the filterOption and I then looked for changes to filterOptions from the filterData which will update it for filterOptions inside productData function. I then added an if statement to switch url for my fetch request in order to either get the default data or category specific data. I added fucntion to sort the data, get selected category and sortProducts.

--- src/main.js
+++ src/main.js
@@ -1,28 +1,49 @@
 const productData = () => {
     return {
         products: [],
+        filteredProducts: [],
         categories: [],
         loading: true,
         error: null,
         showModal: false,
         modalProduct: {},
+        sortOption: 'default',
+        filterOption: 'All categories',
+
 
         init() {
-            this.fetchProducts();
+            this.fetchProducts(this.filterOption);
             this.fetchCategories();
+
+            this.$watch('filterOption', (category) => {
+                this.fetchProducts(category);
+            });
+
+            document.addEventListener('category-changed', (event) => {
+                const category = event.detail;
+                this.filterOption = category;
+                this.fetchProducts(category);
+            });
         },
 
-        async fetchProducts() {
+        async fetchProducts(category) {
             try{
-                const response = await fetch("https://fakestoreapi.com/products");
+                let url = "https://fakestoreapi.com/products"
+                if (category && category !== 'All categories') {
+                    url = `https://fakestoreapi.com/products/category/${category}`
+                }
+                const response = await fetch(url);
                 if (!response.ok) {
                     throw new Error(
                         "Data fetching failed, please check your network connection"
                     )
                 }
+                console.log(url)
                 const data = await response.json();
                 console.log(data)
                 this.products = data;
+                this.filteredProducts = this.product;
+                this.applyFilters();
                 this.loading = false;
             } catch (err) {
                this.error = "Failed to fetch products";
@@ -71,6 +92,33 @@ const productData = () => {
 
         async closeModal() {
             this.showModal = false;
+        },
+
+        applyFilters() {
+            let filtered = this.products
+
+            if (this.sortOption === 'low') {
+                filtered.sort((a, b) => a.price - b.price);
+            } else if (this.sortOption === 'high') {
+                filtered.sort((a,b) => b.price - a.price)
+            }
+
+            this.filteredProducts = filtered;
+        },
+
+        selectCategory(category) {
+            this.filterOption = category;
+            console.log(this.filterOption)
+            this.fetchProducts(category);
+        },
+
+        sortProducts() {
+            if (this.sortOption === 'default') {
+                this.fetchProducts(this.filterOption);
+            } else {
+                this.applyFilters();
+            }
+            
         }
     }
 }
@@ -84,12 +132,11 @@ const filterData = () => {
         initializeCategories() {
             this.fetchCategories();
         },
-    
+
         async fetchCategories() {
             try {
                 const response = await fetch("https://fakestoreapi.com/products/categories");
-                const data = await response.json();
-                this.categories = data;
+                this.categories = await response.json();
             } catch (error) {
                 this.error = 'Failed to fetch categories';
             }
@@ -97,6 +144,12 @@ const filterData = () => {
 
         toggleDropdown() {
             this.dropdownVisible = !this.dropdownVisible;
+        },
+
+        selectCategory(category) {
+            this.filterOption = category;
+            this.$dispatch('category-changed', category)
+            console.log(this.filterOption)
         }
     }
 }
